2b6c2995e7c35cd91dc6f3a20896c4ae621d907c
edot9241 <e.9241@proton.me>
2024-10-09 23:18:54 +0200

    Remove unnecessary print.go

--- cmd/gocat.go
+++ cmd/gocat.go
@@ -9,6 +9,79 @@ import (
 	"github.com/edot9241/gocat/v1/internal"
 )
 
+const versionString = `gocat 1.00
+Copyright (C) E.#9241 <https://github.com/edot9241>.
+License GPLv3+: GNU GPL version 3 or later <https://gnu.org/licenses/gpl.html>.
+This is free software: you are free to change and redistribute it.
+There is NO WARRANTY, to the extent permitted by law.
+
+Written by E.#9241 <https://github.com/edot9241>`
+
+const helpString = `NAME
+       gocat - concatenate files and print on the standard output
+
+SYNOPSIS
+       gocat [OPTION]... [FILE]...
+
+DESCRIPTION
+
+       Concatenate FILE(s) to standard output.
+
+       With no FILE, or when FILE is -, read standard input.
+
+       -A, --show-all
+              equivalent to -vET
+
+       -b, --number-nonblank
+              number nonempty output lines, overrides -n
+
+       -e     equivalent to -vE
+
+       -E, --show-ends
+              display $ at end of each line
+
+       -n, --number
+              number all output lines
+
+       -s, --squeeze-blank
+              suppress repeated empty output lines
+
+       -t     equivalent to -vT
+
+       -T, --show-tabs
+              display TAB characters as ^I
+
+       -u     (ignored)
+
+       -v, --show-nonprinting
+              use ^ and M- notation, except for LFD and TAB
+
+       --help display this help and exit
+
+       --version
+              output version information and exit
+	
+EXAMPLES
+       gocat f - g
+              Output f's contents, then standard input, then g's
+              contents.
+
+       gocat    Copy standard input to standard output.
+
+AUTHOR
+	Written by E.#9241 <https://github.com/edot9241>
+
+REPORTING BUGS
+	Github repository:
+	<https://github.com/edot9241/gocat>
+
+COPYRIGHT
+	Copyright © 2024 E.#9241 <https://github.com/edot9241>.  License GPLv3+:
+	GNU GPL version 3 or later <https://gnu.org/licenses/gpl.html>.
+	This is free software: you are free to change and redistribute
+	it.  There is NO WARRANTY, to the extent permitted by law.
+`
+
 // Parse command line arguments, execute gocat logic on the input
 // and print to output.
 func Run(args []string, output io.Writer) {
@@ -20,12 +93,12 @@ func Run(args []string, output io.Writer) {
 	}
 
 	if config.ShowHelp {
-		internal.PrintHelp(output)
+		fmt.Fprint(output, helpString)
 		return
 	}
 
 	if config.ShowVersion {
-		internal.PrintVersion(output)
+		fmt.Fprint(output, versionString)
 		return
 	}
 
